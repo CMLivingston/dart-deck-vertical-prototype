a628bfff5c3a705ffd72ca76f0bb4ba7bda16367
ngolini22 <nicholas.j.golini.17@dartmouth.edu>
2017-05-15 12:47:52 -0400

    updates

--- imports/components/Home.jsx
+++ imports/components/Home.jsx
@@ -1,18 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
-<<<<<<< HEAD
 import './FormInputStyles.css';  
-=======
-import { browserHistory } from 'react-router';
-import './HomeStyles.css';  
->>>>>>> master
 
 export default class Splash extends Component {
-
-  goToDeckEditor() {
-    browserHistory.push('/deckeditor');
-  }
-
   render() {
     return (
       <div>
@@ -20,7 +10,6 @@ export default class Splash extends Component {
           DartDeck - Home
         </h2>
         <hr></hr>
-<<<<<<< HEAD
           <form className = "formPanel">
             <label> Form Input </label>
             <br></br>
@@ -31,33 +20,10 @@ export default class Splash extends Component {
               :
               <input type="text" name="fback" placeholder="(back of card)"/> 
             </div>
-
           </form>
 
           <button type="button" className="submit" align="center"> Create Deck </button> 
 
-=======
-        <table className="homeTableWrapper">
-          <caption> ALL DECKS </caption> 
-          <tr> 
-            <th>My Decks</th> 
-            <th>Public Decks</th> 
-          </tr> 
-          <tr className="tableContent"> 
-            <td>Biol 12</td> 
-            <td>Cosc 31</td> 
-          </tr> 
-          <tr> 
-            <td>Cosc 30</td> 
-            <td>Math 20</td> 
-          </tr> 
-          <tr> 
-            <td>Math 23</td> 
-            <td>Psyc 06</td> 
-          </tr> 
-        </table> 
-        <button type="button" onClick={this.goToDeckEditor.bind(this)}>Add Deck!</button>
->>>>>>> master
       </div>
     );
   }
